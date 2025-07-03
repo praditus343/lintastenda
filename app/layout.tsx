300e2bf6003ec9a14c980b0da4f82ed8b6a6d281
praditus343 <egidanuarta17@gmail.com>
2025-07-03 12:35:04 +0700

    feat: add Google Analytics integration to track user interactions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'lintas tenda',
@@ -14,6 +15,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Tag */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-TQJN13ZCHS" 
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TQJN13ZCHS');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
